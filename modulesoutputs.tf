cb95a0660731711e0fd240a407925e91f036e66c
badugupadmaja571-glitch <badugupadmaja571@gmail.com>
2026-06-17 12:02:01 +0530

    Refactor outputs to use VPC module values

--- modulesoutputs.tf
+++ modulesoutputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
 
 output "subnet_id" {
-  value = aws_subnet.public.id
+  value = module.vpc.subnet_id
 }
 
 output "igw_id" {
-  value = aws_internet_gateway.igw.id
+  value = module.vpc.igw_id
 }
